d77b42bc76cfb25eb48c27712f7f218ac02f88f6
Thiago Lima <tlima7601@gmail.com>
2026-01-16 14:31:19 -0300

    ler mais implementado

--- react-axios/src/routes/Post/Post.jsx
+++ react-axios/src/routes/Post/Post.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import api from "../../axios/config";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import "./Post.css";
 
 const Post = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { id } = useParams();
+  const [post, setPost] = useState({});
 
-export default Post
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await api.get(`/posts/${id}`);
+        setPost(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar o post:", error);
+      }
+    };
+
+    fetchPost();
+  }, [id]);
+
+  return (
+    <div className="post-container">
+      {!post.title ? (
+        <p>Carregando...</p>
+      ) : (
+        <div className="post">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Post;
